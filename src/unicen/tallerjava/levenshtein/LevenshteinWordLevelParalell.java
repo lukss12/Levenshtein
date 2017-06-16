5ade7423a6f492bc87d98de379a5143e87075814
Lucas Picchi <picchi.lucas@gmail.com>
2017-06-16 16:32:28 -0300

    Update some comments
    Signed-off-by: Lucas Picchi <picchi.lucas@gmail.com>

--- src/unicen/tallerjava/levenshtein/LevenshteinWordLevelParalell.java
+++ src/unicen/tallerjava/levenshtein/LevenshteinWordLevelParalell.java
@@ -6,7 +6,11 @@ import java.util.concurrent.RecursiveTask;
 /**
  * Created by lucas on 15/06/17.
  */
+
+/*Esta clase busca paralelizar el trabajo usando el metodo fork-join*/
 public class LevenshteinWordLevelParalell extends RecursiveTask<Integer> {
+    //Este "Limita Secuencial" puede irse ajustando para encontrar una division del trabajo
+    //mas uniforme
     private static final long SEQUENTIAL_THRESHOLD = 10000000000l;
 
     private List<LevenshteinSentence> sentences;
@@ -23,7 +27,10 @@ public class LevenshteinWordLevelParalell extends RecursiveTask<Integer> {
         this.trim = trim;
     }
 
-    //sum c - i,i=a to b
+    //Esta sumatoria es util para estimar la carga de trabajo en un intervalo (sum c - i,i=a to b)
+    //Cuenta la cantidad de pares a comparar en un intervalo dado donde "c" es el total de oraciones
+    //a y b el comienzo y fin del intervalo y se debe comparar cada oracion del intervalo con las siguientes
+    //oraciones hasta el final
     public static long summation(long a, long b, long c){
         return (a - b - 1l) * (a + b - 2*c) / 2;
     }
